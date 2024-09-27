2038452b42f7796c50d60fa9685aa0abe16fbd5f
matheus ferraz <matheus.ferraz@gmail.com>
2024-09-27 16:22:49 -0300

    .

---
+++ extract_text.js
@@ -0,0 +1,28 @@
+const pdfUtil = require('pdf-to-text');
+const fs = require('fs');
+
+const extract_text = async (stream) => {
+    return new Promise((resolve, reject) => {
+        pdfUtil.pdfToText(stream, (err, data) => {
+            if (err) {
+                console.error('Erro ao extrair texto do PDF:', err);
+                return reject(err);
+            }
+
+            const cleanedData = data
+                .replace(/[^\x20-\x7E]/g, '')
+                .replace(/\s{2,}/g, ' ');
+
+       
+            fs.unlink(stream, (unlinkErr) => {
+                if (unlinkErr) {
+                    console.error('Erro ao deletar arquivo temporário:', unlinkErr);
+                }
+            });
+
+            resolve(data); 
+        });
+    });
+};
+
+module.exports = { extract_text };
--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-var pdfUtil = require('pdf-to-text');
-var pdf_path = "1.pdf";
- 
-pdfUtil.pdfToText(pdf_path, function(err, data) {
-    if (err) throw(err);
-    console.log(data); //print all text    
-  });
+const http = require('http');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const { extract_text } = require('./extract_text');
+
+const port = 8081;
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/pdf_to_text') {
+        let body = '';
+
+        req.on('data', chunk => body += chunk);
+        req.on('end', () => {
+            const pdf_url = JSON.parse(body).data.url;
+            const pdf_file = path.join(__dirname, `temp_${Date.now()}.pdf`);
+
+            https.get(pdf_url, response => {
+                response.pipe(fs.createWriteStream(pdf_file).on('finish', async () => {
+                    try {
+                        const text = await extract_text(pdf_file);
+                        res.writeHead(500, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify({ text }));
+                    } catch {
+                        res.writeHead(500).end(JSON.stringify({ error: 'Failed to extract text' }));
+                    }
+                }));
+            }).on('error', () => {
+                res.writeHead(500).end(JSON.stringify({ error: 'Failed to download PDF' }));
+            });
+        });
+    } else {
+        res.writeHead(404).end(JSON.stringify({ error: 'Not Found' }));
+    }
+});
+
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
